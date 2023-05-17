245a6db3b52470a23a87712e2368d6e030e2fa73
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-17 17:57:12 -0300

    ✅ TEST: createUserWithGoogleUseCase tests

---
+++ src/app/useCases/loginUserWithGoogle.spec.ts
@@ -0,0 +1,55 @@
+import { InMemoryUserRepository } from "../../../test/repositories/user"
+import { InMemoryGoogleOauth2 } from "../../../test/services/google"
+import { UserNotExistsError } from "../../domain/errors/user/userNotExists"
+import { TokenExpiredError } from "../errors/auth/tokenExpired"
+import { CreateUserWithGoogleUseCase } from "./createUserWithGoogle"
+import { LoginUserWithGoogleUseCase } from "./loginUserWIthGoogle"
+
+describe("createUserWithGoogleUseCase tests", function(){
+
+    const repo = new InMemoryUserRepository()
+    const google = new InMemoryGoogleOauth2()
+    
+    const createUser = new CreateUserWithGoogleUseCase(repo,google)
+
+    it("should be login with user already exists", async function(){
+        await createUser.exec({
+            access_token:"valid_token"
+        })
+
+        const sut = new LoginUserWithGoogleUseCase(repo,google)
+
+        const res = await sut.exec({
+            access_token:"valid_token"
+        })
+
+        expect(res.right).not.toBeUndefined()
+        expect(res.right).toHaveProperty("user")
+        expect(res.right?.user).toHaveProperty("id")
+        expect(res.right?.user).toHaveProperty("name")
+        expect(res.right?.user).toHaveProperty("email")
+    })
+
+    it("should be able return error if try login with user not exists", async function(){
+        const sut = new LoginUserWithGoogleUseCase(repo,google)
+
+        const res = await sut.exec({
+            access_token:"another_token"
+        })
+
+        expect(res.left).not.toBeUndefined()
+        expect(res.left).toBeInstanceOf(UserNotExistsError)
+    })
+
+    it("should be able return error with invalid_token", async function(){
+        const sut = new LoginUserWithGoogleUseCase(repo,google)
+
+        const res = await sut.exec({
+            access_token:"invalid_token"
+        })
+
+        expect(res.left).not.toBeUndefined()
+        expect(res.left).toBeInstanceOf(TokenExpiredError)
+    })
+
+})

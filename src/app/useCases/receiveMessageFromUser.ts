2ca5443c72a2bbea93782ddff26062fff73f892d
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 01:11:48 -0300

    📦 FEAT: ReceiveMessageFromUserUseCase implemented

---
+++ src/app/useCases/receiveMessageFromUser.ts
@@ -0,0 +1,24 @@
+import { MessagePublishInterface } from "../../domain/entities/messagePublish";
+import { Either, Left, Right } from "../../domain/errors/either";
+import { ErrorBase } from "../../domain/errors/errorBase";
+import { ReceiveMessageFromUserUseCaseInterface } from "../../domain/useCases/receiveMessageFromUser";
+import { PublishSubscribeMessageInterface } from "../interfaces/services/publishMessage";
+
+export class ReceiveMessageFromUserUseCase implements ReceiveMessageFromUserUseCaseInterface{
+
+    constructor(
+        private readonly messagePublisher:PublishSubscribeMessageInterface
+    ){}
+    
+    async receiveMessage(message:MessagePublishInterface):Promise<Either<ErrorBase,void>>{
+        console.log("Message receive :",message)
+        return Right.create(undefined)
+    }
+
+    async exec () : Promise<Either<ErrorBase, void>>{
+        const receive = await this.messagePublisher.receive({receveFunc:this.receiveMessage})
+        if(receive.left) return Left.create(receive.left)
+        return Right.create(undefined)
+    }
+
+}

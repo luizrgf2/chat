b8a490cbfb87ee2b39d9d8e7288f72b3a9e1477f
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 12:19:01 -0300

    📦 FEAT: implemente new interface

--- src/app/useCases/sendMessageToUser.ts
+++ src/app/useCases/sendMessageToUser.ts
@@ -1,38 +1,41 @@
-import { MessagePublishEntity } from "../../domain/entities/messagePublish";
+import { MessagePublishEntity, MessagePublishInterface } from "../../domain/entities/messagePublish";
 import { Either, Left, Right } from "../../domain/errors/either";
 import { ErrorBase } from "../../domain/errors/errorBase";
 import { SendMessageToUserUseCaseInput, SendMessageToUserUseCaseInterface, SendMessageToUserUseCaseOutput} from "../../domain/useCases/sendMessageToUser"
+import { UserRepositoryInterface } from "../interfaces/repository/user";
 import { PublishSubscribeMessageInterface } from "../interfaces/services/publishMessage";
 
 
 export class SendMessageToUserUseCase implements SendMessageToUserUseCaseInterface{
     
     constructor(
-        private readonly messagePublisher:PublishSubscribeMessageInterface
+        private readonly messagePublisher:PublishSubscribeMessageInterface,
+        private readonly userRepo:UserRepositoryInterface
     ){}
 
-    async sendMessage({message}:SendMessageToUserUseCaseInput):Promise<Either<ErrorBase,void>>{
+    async sendMessage(message:MessagePublishInterface):Promise<Either<ErrorBase,void>>{
         const send = await this.messagePublisher.send(message)
         if(send.left) return Left.create(send.left)
         return Right.create(undefined)
     }
     
     async exec(input: SendMessageToUserUseCaseInput) : Promise<Either<ErrorBase, SendMessageToUserUseCaseOutput>>{
-        const {message:{idUser,message,userName}} = input
+        const {message,idUser} = input
+
+        const userOrError = await this.userRepo.findUserById(idUser)
+        if(userOrError.left) return Left.create(userOrError.left)
 
         const messageData = MessagePublishEntity.create({
-            idUser,
-            message,
-            userName
+            idUser:userOrError.right.id,
+            userName:userOrError.right.name,
+            message:message
         })
         if(messageData.left) return Left.create(messageData.left)
 
         const send = await this.sendMessage({
-            message:{
-                idUser,
-                message,
-                userName
-            }
+            idUser:userOrError.right.id,
+            message:message,
+            userName:userOrError.right.name
         })        
         if(send.left) return Left.create(send.left)
 

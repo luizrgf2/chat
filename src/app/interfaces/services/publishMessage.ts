05396b5ac5ff7e8f008a0618456e21761c331e16
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 01:12:06 -0300

    📦 FEAT: set input

--- src/app/interfaces/services/publishMessage.ts
+++ src/app/interfaces/services/publishMessage.ts
@@ -2,7 +2,11 @@ import { MessagePublishInterface } from "../../../domain/entities/messagePublish
 import { Either } from "../../../domain/errors/either";
 import { ErrorBase } from "../../../domain/errors/errorBase";
 
+export interface ReceiveMessageInput{
+    receveFunc:(message:MessagePublishInterface)=>Promise<Either<ErrorBase,void>>
+}
+
 export interface PublishSubscribeMessageInterface{
     send:(message:MessagePublishInterface)=>Promise<Either<ErrorBase,void>>
-    receive:()=>Promise<Either<ErrorBase,void>>
+    receive:(input:ReceiveMessageInput)=>Promise<Either<ErrorBase,void>>
 }

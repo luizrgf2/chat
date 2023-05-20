988f27fdf364607a882154cc756be794dad781c7
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 00:53:23 -0300

    📦 FEAT: receive method implemented

--- src/infra/services/publishMessage.ts
+++ src/infra/services/publishMessage.ts
@@ -1,15 +1,15 @@
 import { Channel } from "amqplib";
-import { PublishMessageInterface } from "../../app/interfaces/services/publishMessage";
 import { RABBITMQ_EXCHANGE_CHATAPP_NAME } from "../../config";
 import { Either, Left, Right } from "../../domain/errors/either";
 import { ErrorBase } from "../../domain/errors/errorBase";
-import { MessagePublishInterface } from "../../domain/useCases/sendMessageToUser";
 import { RabbitMQConnector } from "./RabbitMqConnection";
+import { PublishSubscribeMessageInterface } from "../../app/interfaces/services/publishMessage";
+import { MessagePublishInterface } from "../../domain/entities/messagePublish";
 
 
 if(!RABBITMQ_EXCHANGE_CHATAPP_NAME) throw new Error("RABBITMQ_EXCHANGE_CHATAPP_NAME not is empty!")
 
-export class PublishMessage implements PublishMessageInterface{
+export class PublishMessage implements PublishSubscribeMessageInterface{
 
     async createExchange(ch:Channel):Promise<Either<ErrorBase,void>>{
         try{
@@ -22,12 +22,24 @@ export class PublishMessage implements PublishMessageInterface{
         }
     }
 
+    async createQueue(ch:Channel):Promise<Either<ErrorBase,string>>{
+        try{
+            const {queue} = await ch.assertQueue("",{
+                exclusive:true,
+                durable:false
+            })
+            return Right.create(queue)
+        }catch(e){
+            return Left.create(new ErrorBase(String(e),500))
+        }
+    }
+
     async sendMessage(ch:Channel,message:MessagePublishInterface):Promise<Either<ErrorBase,void>>{
         try{
             const messageToText = JSON.stringify(message)
 
-            console.log("Message from :"+message.userName)
-            console.log("Message from id:"+message.idUser)
+            console.log("Send message from :"+message.userName)
+            console.log("Send message from id:"+message.idUser)
 
             ch.publish(RABBITMQ_EXCHANGE_CHATAPP_NAME||"","",Buffer.from(messageToText))
             return Right.create(undefined)
@@ -35,6 +47,7 @@ export class PublishMessage implements PublishMessageInterface{
             return Left.create(new ErrorBase(String(e),500))
         }
     }
+
 
     async send({idUser,message,userName}: MessagePublishInterface) : Promise<Either<ErrorBase, void>>{
         const connection = await RabbitMQConnector.connect()
@@ -52,5 +65,42 @@ export class PublishMessage implements PublishMessageInterface{
         return Right.create(undefined)
         
     }
+
+    async receive() : Promise<Either<ErrorBase, void>>{
+        const connection = await RabbitMQConnector.connect()
+        const channel = await connection.createChannel()
+        
+        const exchangeOrError = await this.createExchange(channel)
+        if(exchangeOrError.left) {
+            await channel.close();
+            await connection.close();
+            return Left.create(exchangeOrError.left)
+        }
+
+        const queueOrError = await this.createQueue(channel)
+        if(queueOrError.left) {
+            await channel.close();
+            await connection.close();
+            return Left.create(queueOrError.left)
+        }
+
+        await channel.bindQueue(queueOrError.right,RABBITMQ_EXCHANGE_CHATAPP_NAME||"","")
+
+        channel.consume(queueOrError.right,message=>{
+            if(message){
+                const content = JSON.parse(message.content.toString())
+
+                console.log("Receive message from :"+content.userName)
+                console.log("Receive message from id:"+content.idUser)
+
+                channel.ack(message)
+            }
+
+
+        },{noAck:true})
+
+        return Right.create(undefined)
+    }
+
 
 }

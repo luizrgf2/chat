6d3c8db88680f2c219ed1e2f6663a89f7c27fbff
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 10:59:38 -0300

    📦 FEAT: new route /user/message/send

--- src/main/routes/publicRoutes.ts
+++ src/main/routes/publicRoutes.ts
@@ -2,9 +2,11 @@ import {Router} from "express"
 import { CreateUserWithGoogleExpressController } from "../controllers/createUserWithGoogle"
 import { LoginUserWithGoogleExpressController } from "../controllers/loginUserWithGoogle"
 import { GetAccesTokenFromGoogleExpressController } from "../controllers/getAccessTokenFromGoogle"
+import { SendMessageToUserExpressController } from "../controllers/sendMessageToUser"
 
 export const publicRoutes = Router()
 
 publicRoutes.get("/user/google/:code",GetAccesTokenFromGoogleExpressController.exec)
 publicRoutes.post("/user/register",CreateUserWithGoogleExpressController.exec)
 publicRoutes.post("/user/login", LoginUserWithGoogleExpressController.exec)
+publicRoutes.post("/user/message/send",SendMessageToUserExpressController.exec)

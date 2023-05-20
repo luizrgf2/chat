c83eb3c37703b42a926b2167cbde1f0d7120558d
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 11:45:20 -0300

    📦 FEAT: implemented jwt in useCase

--- src/app/useCases/loginUserWIthGoogle.ts
+++ src/app/useCases/loginUserWIthGoogle.ts
@@ -3,12 +3,14 @@ import { ErrorBase } from "../../domain/errors/errorBase";
 import { LoginWithGoogleUseCaseInput, LoginWithGoogleUseCaseInterface, LoginWithGoogleUseCaseOutput } from "../../domain/useCases/loginUserWithGoogle";
 import { UserRepositoryInterface } from "../interfaces/repository/user";
 import { GoogleOauth2Interface } from "../interfaces/services/google";
+import { JWTInterface } from "../interfaces/services/jwt";
 
 export class LoginUserWithGoogleUseCase implements LoginWithGoogleUseCaseInterface{
 
     constructor(
         private readonly userRepo:UserRepositoryInterface,
-        private readonly googleService:GoogleOauth2Interface
+        private readonly googleService:GoogleOauth2Interface,
+        private readonly jwt: JWTInterface
     ){}
 
     async exec(input: LoginWithGoogleUseCaseInput) : Promise<Either<ErrorBase, LoginWithGoogleUseCaseOutput>>{
@@ -21,8 +23,14 @@ export class LoginUserWithGoogleUseCase implements LoginWithGoogleUseCaseInterfa
 
         if(userExists.left) return Left.create(userExists.left)
 
+
+        const durationInMileconds = 86400000 //24 in mileconds
+        const token =await this.jwt.encode({id:userExists.right.id},durationInMileconds)
+        if(token.left) return Left.create(token.left)
+
         return Right.create({
-            user:{...userExists.right}
+            user:{...userExists.right},
+            token:token.right
         })
     }
 

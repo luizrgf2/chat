89b926d1eebf07da4b105fac20602444656fff5f
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 11:44:13 -0300

    📦 FEAT: InMemoryPublishMessage new method add

--- test/repositories/publisherMessage.ts
+++ test/repositories/publisherMessage.ts
@@ -1,13 +1,17 @@
-import { PublishMessageInterface } from "../../src/app/interfaces/services/publishMessage";
+import { PublishSubscribeMessageInterface, ReceiveMessageInput } from "../../src/app/interfaces/services/publishMessage";
+import { MessagePublishInterface } from "../../src/domain/entities/messagePublish";
 import { Either, Right } from "../../src/domain/errors/either";
 import { ErrorBase } from "../../src/domain/errors/errorBase";
-import { MessagePublishInterface } from "../../src/domain/useCases/sendMessageToUser";
 
-export class InMemoryPublishMessage implements PublishMessageInterface{
-
+export class InMemoryPublishMessage implements PublishSubscribeMessageInterface{
     messages:MessagePublishInterface[] = []
-
+    
+    
     async send ({idUser,message,userName}: MessagePublishInterface) : Promise<Either<ErrorBase, void>>{
+        return Right.create(undefined)
+    }
+   
+    async receive (input: ReceiveMessageInput) :Promise<Either<ErrorBase, void>>{
         return Right.create(undefined)
     }
 }

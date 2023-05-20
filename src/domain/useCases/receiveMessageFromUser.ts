e4eddf4ba9d0217554c902adbe1f2a17fd66f293
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-20 01:11:09 -0300

    🛠  REFACTOR: remove input

--- src/domain/useCases/receiveMessageFromUser.ts
+++ src/domain/useCases/receiveMessageFromUser.ts
@@ -1,12 +1,9 @@
-import { MessagePublishInterface } from "../entities/messagePublish"
 import { Either } from "../errors/either"
 import { ErrorBase } from "../errors/errorBase"
 
-export interface ReceiveMessageFromUserUseCaseInput{
-    message:MessagePublishInterface
-}
+
 
 
 export interface ReceiveMessageFromUserUseCaseInterface{
-    exec:(input:ReceiveMessageFromUserUseCaseInput)=>Promise<Either<ErrorBase,void>>
+    exec:()=>Promise<Either<ErrorBase,void>>
 }

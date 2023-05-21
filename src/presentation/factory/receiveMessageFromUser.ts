c0d3c08d583d8042ce41be2ab5bbe4b9b522a090
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-21 09:17:38 -0300

    🛠  REFACTOR: only usecase

--- src/presentation/factory/receiveMessageFromUser.ts
+++ src/presentation/factory/receiveMessageFromUser.ts
@@ -1,19 +1,13 @@
 import { ReceiveMessageFromUserUseCase } from "../../app/useCases/receiveMessageFromUser";
 import { PublishSubscribeMessage } from "../../infra/services/publishSubscribe";
-import { ReceiveMessageFromUserController } from "../controllers/receiveMessageFromUser";
+import { WebSocket } from "../../infra/services/webSocket";
 
 export class ReceiveMessageFromUserFactory{
 
     static handle(){
         const service = new PublishSubscribeMessage()
-        const userCase = new ReceiveMessageFromUserUseCase(service)
-        const controller = new ReceiveMessageFromUserController(userCase)
-        return controller
-    }
-
-    static handleOnlyUseCase(){
-        const service = new PublishSubscribeMessage()
-        const userCase = new ReceiveMessageFromUserUseCase(service)
+        const service2 = new WebSocket()
+        const userCase = new ReceiveMessageFromUserUseCase(service,service2)
         return userCase
     }
 }

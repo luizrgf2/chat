86b91d4fc06ac4c128677b33b050c477ca1cf813
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-21 11:09:11 -0300

    📦 FEAT: socket io server

---
+++ src/infra/utils/websocketsConnection.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = http.createServer(app);
+
+export const io = new Server(server);
+
+
+
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Servidor Socket.IO iniciado em http://localhost:${port}`);
+});

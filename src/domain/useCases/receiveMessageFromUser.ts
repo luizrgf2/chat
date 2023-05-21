24a80524edff8a050e8f05b376ef25884a0f4d53
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-21 14:45:43 -0300

    🛠  REFACTOR: remove

--- src/domain/useCases/receiveMessageFromUser.ts
+++
@@ -1,9 +0,0 @@
-import { Either } from "../errors/either"
-import { ErrorBase } from "../errors/errorBase"
-
-
-
-
-export interface ReceiveMessageFromUserUseCaseInterface{
-    exec:()=>Promise<Either<ErrorBase,void>>
-}

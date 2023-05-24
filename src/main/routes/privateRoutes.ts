54a53995c3117e842f14f08a514c6028deece5dd
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-24 17:36:27 -0300

    📦 FEAT: add new route /user/find

--- src/main/routes/privateRoutes.ts
+++ src/main/routes/privateRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { SendMessageToUserExpressController } from "../controllers/sendMessageToUser";
 import { PrivateRouteMiddleware } from "../http/expressMiddlewares/privateRoute";
+import { FindUserExpressController } from "../controllers/findUser";
 
 export const privateRoutes = Router()
 privateRoutes.use(PrivateRouteMiddleware)
+
 privateRoutes.post("/user/message/send",SendMessageToUserExpressController.exec)
+privateRoutes.post("/user/find",FindUserExpressController.exec)

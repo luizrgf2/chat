7e73fb1b2e81851c01a939366d549940e6b70ba6
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-24 16:25:58 -0300

    📦 FEAT: findUserUseCaseInterface add

---
+++ src/domain/useCases/findUser.ts
@@ -0,0 +1,15 @@
+import { UserInterface } from "../entities/user"
+import { Either } from "../errors/either"
+import { ErrorBase } from "../errors/errorBase"
+
+export interface FindUserUseCaseInput{
+    idUser:string
+}
+
+export interface FindUserUseCaseOutput{
+    user:UserInterface
+}
+
+export interface FindUserUseCase{
+    exec:(input:FindUserUseCaseInput)=>Promise<Either<ErrorBase,FindUserUseCaseOutput>>
+}
